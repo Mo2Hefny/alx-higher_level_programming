316ccfa57984d7431528132f31b7b8afc790a42d
Mo2Hefny <moamensaied1@gmail.com>
2022-12-05 14:07:22 -0800

    13

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -17,6 +17,9 @@ int n;
 struct listint_s *next;
 } listint_t;
 
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 void reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
 
