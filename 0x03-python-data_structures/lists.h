36e9cd2939904d1785078c6512560e9e79b50574
Mo2Hefny <moamensaied1@gmail.com>
2022-12-05 14:06:08 -0800

    13

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ */
+
+typedef struct listint_s
+{
+int n;
+struct listint_s *next;
+} listint_t;
+
+void reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+
+#endif
