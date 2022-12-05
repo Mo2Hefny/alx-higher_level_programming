fc8ff6997937aa20c9b3e8a6907b4129e9608773
Mo2Hefny <moamensaied1@gmail.com>
2022-12-05 13:52:31 -0800

    8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    tpl = ()
+    if len(sentence) == 0:
+        tpl = 0, "None"
+    else:
+        tpl = len(sentence), sentence[0]
+    return tpl
