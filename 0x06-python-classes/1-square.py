6b4e59ddb9e8ed3375154a963d90a9ab4ea81f30
Mo2Hefny <moamensaied1@gmail.com>
2022-12-20 15:52:43 -0800

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" this module defines a class Square"""
+
+
+class Square:
+    """ class for Square"""
+    def __init__(self, size):
+        """ Initialization of class """
+        self.__size = size
