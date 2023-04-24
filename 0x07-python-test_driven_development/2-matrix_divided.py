74d0aac474a7044ac8ddf0da40fe273bfa01b001
Mo2Hefny <moamensaied1@gmail.com>
2023-04-24 22:23:38 +0200

    fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,31 +14,25 @@ def matrix_divided(matrix, div):
             div (int|float): An integer or floating point number.
         Returns:  A new matrix.
     """
+    mat_msg = "matrix must be a matrix (list of lists) of integers/floats"
     if div is None or (type(div) is not int and type(div) is not float):
         raise TypeError("div must be a number")
-    
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
     if not matrix or type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+        raise TypeError(mat_msg)
     row_size = 0
     new_mat = []
     for sublists in matrix:
         new_list = []
         if not sublists or type(sublists) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        
+            raise TypeError(mat_msg)
         if row_size != 0 and len(sublists) != row_size:
             raise TypeError("Each row of the matrix must have the same size")
-        
         row_size = len(sublists)
         for num in sublists:
             if type(num) is not int and type(num) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(mat_msg)
             new_list.append(round(num / div, 2))
-
         new_mat.append(new_list)
-
     return new_mat
