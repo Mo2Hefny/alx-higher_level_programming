7738a1eb60e0502fe6be3b2618ea624f0a244a82
Mo2Hefny <moamensaied1@gmail.com>
2023-04-25 00:51:55 +0200

    fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,13 +13,17 @@ class TestMaxInteger(unittest.TestCase):
   def test_repeated_integer(self):
      self.assertEqual(max_integer([5, 100, 100, 3]), 100)
   def test_float_numbers(self):
-      self.assertEqual(max_integer([4.8, 5.9, 5.1, 5]), 5)
+      self.assertEqual(max_integer([4.8, 5.9, 5.1, 5]), 5.9)
+  def test_negative_numbers(self):
+      self.assertEqual(max_integer([-10, -9, -8, -7]), -7)
   def test_big_list(self):
         self.assertEqual(max_integer([
             901, 902, 903, 904, 905, 906, 907, 908, 909, 910,
             911, 912, 913, 914, 915, 916, 917, 918, 919, 920,
             919, 918, 917, 1000, 915, 914, 913, 912, 911, 910,
             909, 908, 907, 906, 905, 904, 903, 902, 901]), 1000)
+  def test_one_number_in_a_list(self):
+      self.assertEqual(max_integer([1]), 1)
   def test_not_a_list(self):
       with self.assertRaises(TypeError):
           max_integer(5)
